282f0c001d7fa24c3eb797214bf94c8b768b34b8
VAIBHAV RANA <vaibhavrana0204@gmail.com>
2026-05-12 20:23:01 +0530

    folder structure updated

--- next-client/app/(main)/layout.tsx
+++
@@ -1,8 +0,0 @@
-import DashboardLayout from '@/components/DashboardLayout';
-import React from 'react';
-
-const layout = ({ children }: { children: React.ReactNode }) => {
-  return <DashboardLayout>{children}</DashboardLayout>;
-};
-
-export default layout;
